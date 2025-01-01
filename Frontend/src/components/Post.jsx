a0c6f5b53c2458fd89b29b7f8179909150ad3316
sakkarin1202 <111554817+sakkarin1202@users.noreply.github.com>
2025-01-01 14:09:00 +0700

    Update Post.jsx

--- Frontend/src/components/Post.jsx
+++ Frontend/src/components/Post.jsx
@@ -5,22 +5,25 @@ const Post = ({ title, author, summary, cover, createdAt, _id }) => {
   return (
     <>
       <div className="card card-side bg-base-100 shadow-xl">
-        <figure className="md:1/2 flex items-center">
-          <a href={"/post/" + _id} className="href">
-            <img src={`${baseURL}/${cover}`} alt={title} />
-          </a>
+        <figure className="md:1/2 flex items-center justify-center">
+          <img
+            src={`${baseURL}/${cover}`}
+            alt="title"
+            className="w-full h-64 object-cover"
+          />
         </figure>
-        <div className="p-6 md:1/2 flex flex-col justify-between card-body">
-          <a href={"/post/" + _id} className="href"></a>
-          <h2 className="card-title">{title}</h2>
-          <p>
-            {author.username}-{createdAt}
-          </p>
-          <p>{summary}</p>
+        <div className="p-6 md:1/2 flex flex-col card-body">
+          <a href={"/post/" + _id} className="href">
+            <h2 className="card-title">{title}</h2>
+          </a>
+          <h4>
+            {author.username} - {createdAt}
+          </h4>
+          <h4>{summary}</h4>
         </div>
       </div>
     </>
-  );
+  );;
 };
 
 export default Post;
