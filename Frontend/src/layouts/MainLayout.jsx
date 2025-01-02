d36b874ce538b58096e5c5fc85ab3a9a54e185e4
sakkarin1202 <xfake12340@hotmail.com>
2025-01-02 10:14:49 +0700

    updatePost

--- Frontend/src/layouts/MainLayout.jsx
+++ Frontend/src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/Navbar";
 import React from "react";
 import Footer from "../components/Footer";
 
