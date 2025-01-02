67f68947c7c3e57196f25b02dc6c8aabe6768cd4
sakkarin1202 <xfake12340@hotmail.com>
2025-01-02 10:33:38 +0700

    updatePost

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Welcom to SE NPRU Blog resfull api</h1>");
 });
 
-app.use("/uploads",express.static(__dirname+"/uploads"))
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/post", postRouter);
 app.listen(PORT, () => {
