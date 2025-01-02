9340bdb09e1a0578dcbc84cdb8ef063e521af8f3
sakkarin1202 <xfake12340@hotmail.com>
2025-01-02 10:16:41 +0700

    updatePost

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,36 +1,36 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import PostService from "../services/post.service";
 import Swal from "sweetalert2";
-import Post from "../components/post";
+import Post from "../components/Post";
 const Home = () => {
-  const [posts,setPosts] =useState([])
-  useEffect(()=>{
-    const fetchPosts = async()=>{
-      try{
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
         const response = await PostService.getPosts();
-        if(response.status===200){
-          setPosts(response.data)
+        if (response.status === 200) {
+          setPosts(response.data);
         }
-      }catch(error){  
+      } catch (error) {
         Swal.fire({
-        title: "Home",
-        text: error?.response?.data?.message || error.message,
-        icon: "error",
-      });
+          title: "Home",
+          text: error?.response?.data?.message || error.message,
+          icon: "error",
+        });
       }
-    }
+    };
     fetchPosts();
-  },[])
+  }, []);
   return (
-  <>
-    <div className="flex flex-col space-y-6">
-      {posts.length > 0 && posts.map((post,index) => {
-        return <Post key={index} {...post} />;  
-      })}
-    </div>
-  </>
-);
-
+    <>
+      <div className="flex flex-col space-y-6">
+        {posts.length > 0 &&
+          posts.map((post, index) => {
+            return <Post key={index} {...post} />;
+          })}
+      </div>
+    </>
+  );
 };
 
 export default Home;
